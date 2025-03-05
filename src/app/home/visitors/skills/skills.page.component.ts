cead928749a1af15db9392644ae2f3c64441b83c
djoudj-dev <nedellec.julien.78@gmail.com>
2025-03-05 21:24:09 +0100

    feat(components): Create base page components for portfolio website
    - Add Home, About, Contact, Projects, and Skills page components
    - Implement basic component structure with HTML templates
    - Include corresponding spec files for testing

---
+++ src/app/home/visitors/skills/skills.page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './skills.page.component.html',
+  styleUrl: './skills.page.component.css'
+})
+export class SkillsPageComponent {
+
+}
