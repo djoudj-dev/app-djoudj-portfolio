f3b4c77f1e05a3353cc263f97ba71868c63c98a3
djoudj-dev <nedellec.julien.78@gmail.com>
2025-03-25 21:18:38 +0100

    feat(app): Ajoute le composant RouterOutlet pour la gestion des routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { NavbarSmartComponent } from './core/components/navbar/navbar.smart.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [NavbarSmartComponent]
+  imports: [NavbarSmartComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'Julien N.';
