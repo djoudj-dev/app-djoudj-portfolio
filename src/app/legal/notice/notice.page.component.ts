b4565d1c34b158df09a81f7609f5c37f703706a7
djoudj-dev <nedellec.julien.78@gmail.com>
2025-03-28 18:09:37 +0100

    feat(component:pages): Ajoute la page des Mentions Légales
    - Implémente le composant `NoticePageComponent` avec sa structure HTML et ses styles de base.
    - Inclut les sections officielles : Éditeur, Hébergement, Propriété Intellectuelle, et plus.

---
+++ src/app/legal/notice/notice.page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-notice',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './notice.page.component.html',
+  styleUrl: './notice.page.component.css'
+})
+export class NoticePageComponent {
+  // Add any component logic here
+}
