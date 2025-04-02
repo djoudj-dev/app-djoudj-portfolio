69d69f8f541f903f1ebc1ade482a33bccc3e0258
djoudj-dev <nedellec.julien.78@gmail.com>
2025-04-02 23:07:31 +0200

    chore(config): Supprime des fichiers obsolètes et corrige une typo
    - Supprime les fichiers inutilisés liés à Supabase et le fichier `config.toml`.
    - Corrige une faute de frappe dans un commentaire du fichier global-setup.

--- backend-e2e/src/support/global-setup.ts
+++ backend-e2e/src/support/global-setup.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 module.exports = async function () {
-  // Start services that that the app needs to run (e.g. database, docker-compose, etc.).
+  // Start services that the app needs to run (e.g. database, docker-compose, etc.).
   console.log('\nSetting up...\n');
 
   // Hint: Use `globalThis` to pass variables to global teardown.
